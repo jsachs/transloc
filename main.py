001b2b1be09e70a4baeb9aeddf051ac3d3a1f668
Jacob Sachs <dicechucker@gmail.com>
2013-02-09 15:21:21 -0600

    added support for Google Maps API geocoding in main

--- main.py
+++ main.py
@@ -10,11 +10,26 @@ from route_finder import get_directions
 
 
 if __name__ == '__main__':
-    dest = raw_input("Please enter destination address:")
-    dest.replace(' ','+')
-    address = 'address=' + dest
+    d  = raw_input("Please enter destination address:")
+    s = raw_input("Please enter start address:")
+    dest = []
+    start = []
 
     url = 'http://maps.googleapis.com/maps/api/geocode/json?'
-    address_url = url + address
+    
+    for loc, point in zip([s, d],[start, dest]):
+        loc = loc.replace(" ","+")
+        address = 'address=' + loc
+    
+        address_url = url + address + "&sensor=false"
+        print address_url
+        address_response = urlopen(address_url)
+        json_address = load(address_response)
 
-    print get_directions((41.792109,-87.600072),(41.796438,-87.605125))
+        lat = json_address['results'][0]['geometry']['location']['lat']
+        lng = json_address['results'][0]['geometry']['location']['lng']
+        point.append(lat)
+        point.append(lng)
+
+
+    print get_directions((start[0],start[1]), (dest[0],dest[1]))
